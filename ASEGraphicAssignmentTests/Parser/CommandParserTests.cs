7984cdb2d7d9980d15488db1730f832e87085109
CavneyMP01 <matthewcavney@yahoo.co.uk>
2023-11-14 17:17:28 +0000

    Command Parser Tests Passing

---
+++ ASEGraphicAssignmentTests/Parser/CommandParserTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASEGraphicAssignment.Parser;
+using System;
+using System.Linq;
+
+namespace ASEGraphicAssignment.Tests
+{
+    [TestClass()]
+    public class CommandParserTests
+    {
+        // Single Line Correctly Parsed
+        [TestMethod() ]
+
+        public void ParseSingleLineCommandWithParametersTest()
+        {
+            // Creating a new instance of command parser and a string input "drawto 100, 100" 
+            var parser = new CommandParser();
+            string input = "moveto 100 100";
+
+            // 
+            var commandResult = parser.ParseCommand(input);
+            string[] tokens = input.Split(' ');
+
+            // Check that the tokens match what they should with the above command
+            Assert.IsNotNull(commandResult, "Command result should not be null.");
+            Assert.AreEqual("moveto", tokens[0], "First token did not match command.");
+            Assert.AreEqual("100", tokens[1], "Second token does not match expected param.");
+            Assert.AreEqual("100", tokens[2], "Third token does not match expected param.");
+        }
+
+        // multiLine parsed correctly
+        [TestMethod()]
+        public void ParseMultiLineCommandsTest()
+        {
+            // New instance of command parser and a string to act as user input split into two lines.
+            var parser = new CommandParser();
+            string multiLineInput = "moveto 100 100\nreset";
+
+            // Splitting commands up by new line and removing empty entries then passing lines into a foreach loop to itterate over the lines
+            string[] lines = multiLineInput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var commandResult = parser.ParseCommand(line);
+                string[] tokens = line.Split(' ');
+
+                // check there are more than two lines annd is not null
+                Assert.IsNotNull(commandResult, "Command result should not be null: " + line);
+                Assert.IsTrue(tokens.Length >= 1, "There should be at least one command: " + line);
+            }
+        }
+    }
+}
